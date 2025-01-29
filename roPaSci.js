a39b00c762208010415645732e1199ead0555274
Nicolas Zarakas <nzarakas1@gmail.com>
2025-01-29 20:43:11 +0200

    added DOM methods to create buttons for r/p/s, added divs that trigger to announce who won round, score, final winner deleted previous functions e.g. playgame()

--- roPaSci.js
+++ roPaSci.js
@@ -1,85 +1,91 @@
+//function that randomly selects rock paper or scissors as the computer choice
 function getComputerChoice() {
     let a = Math.floor(Math.random()*3) +1;
     return a===1 ? `rock`: a===2? `paper` : `scissors`;
 }
 
-function getHumanChoice(){
-    let humanChoiceReg = prompt("Rock Paper or Scissors?");
-    const regRock = /^rock$/i;
-    const regPaper = /^paper$/i;
-    const regScissors = /^scissors$/i;
+//playerButtons is an Array
+const playerButtons = document.querySelectorAll("button") 
 
-    if (humanChoiceReg.match(regRock)) {
-        return `rock`;
-    }
-    else if (humanChoiceReg.match(regPaper)) {
-        return `paper`;
-    }
-    else if (humanChoiceReg.match(regScissors)) {
-        return `scissors`;
-    }
-    else alert("Please select one of the options rock/paper/scissors");
-}
+//Div creation for score display
+const container = document.querySelector("body");
+
+//Div that announces who won the Round
+const winsDiv = document.createElement("div");
+container.appendChild(winsDiv);
+
+//Div that announces the current score after playing a Round
+const scoreDiv = document.createElement("div");
+container.appendChild(scoreDiv);
+
+//create div that will trigger as soon as there is a winner to announce with the finalscore
+const finalScoreDiv = document.createElement("div");
+container.appendChild(finalScoreDiv);
+
+
+playerButtons.forEach(button =>{ //every button gets its own event handler
+    button.addEventListener("click", function(e) {
+        
+        let testChoice = e.target.id;
+        let computerChoice = getComputerChoice();
+        playRound(testChoice,computerChoice);
+        
+        finalScoreDiv.textContent = '';
+
+        if (humanScore === 5 || computerScore === 5){
+            
+            finalScoreDiv.textContent = 
+                humanScore === 3  ? 
+                `You win the Game! Finai Score is You:${humanScore} Computer:${computerScore}`:
+                `Computer wins the Game... Final Score is You:${humanScore} Computer:${computerScore}`;
+            
+            resetScores();
+        }
+
+    });
+});
 
 let humanScore = 0;
 let computerScore = 0;
 
+resetScores = () =>{
+    humanScore = 0;
+    computerScore = 0;
+}
+
 
 function playRound(humanChoice, computerChoice){
-
-    
+       
     let choiceCombo = humanChoice + computerChoice ;
     
-    switch(choiceCombo){
-        
+    switch(choiceCombo){      
         case `paperrock`:
         case `scissorspaper`:
         case `rockscissors`:
-            console.log(`Human wins! ${humanChoice} beats ${computerChoice}`);
             humanScore++;
+            winsDiv.textContent = `Human wins the Round! ${humanChoice} beats ${computerChoice}\r\n`;
+            scoreDiv.textContent = `The Score is Human: ${humanScore} Computer: ${computerScore}`;
             break;
 
         case `scissorsrock`:
         case `paperscissors`:
         case `rockpaper`:
-            console.log(`Computer wins! ${humanChoice} loses to ${computerChoice}`);
             computerScore++;
+            winsDiv.textContent = `Computer wins the Round! ${humanChoice} loses to ${computerChoice}\r\n`;
+            scoreDiv.textContent = `The Score is Human: ${humanScore} Computer: ${computerScore}`;
             break;
+
         case `scissorsscissors`:
         case `rockrock`:
         case `paperpaper`:
-            console.log(`It's a Tie...Both players chose ${computerChoice}`);
+            winsDiv.textContent = `It's a Tie...Both players chose ${computerChoice}\r\n`;
+            scoreDiv.textContent = `The Score is Human: ${humanScore} Computer: ${computerScore}`;
             break;
+
         default:
             console.log("Something didn't quite go right..");
     }
-
 }
-function playGame() {
-    
-    let winner = 0;
-    while(!winner){
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-    
-        playRound(humanSelection, computerSelection);
-        console.log('humanScore:'+humanScore +'   computerScore:'+computerScore );
-    
-        if(humanScore === 3) {
-            console.log("You win!")
-            winner = 1;
-        }
-        else if (computerScore === 3) {
-            console.log("Computer wins..")
-            winner = 1;
-        }
-    }
-    winner = 0;
-    humanScore = 0;
-    computerScore = 0;
-}
-
-playGame();
 
 
 
